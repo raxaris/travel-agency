74c1f53d4b694c8e1970395f085a2f95de43cb53
raxar <ansar.utenov@gmail.com>
2024-02-27 16:53:12 +0600

    microfix(v3)

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -3,8 +3,20 @@ const router = express.Router();
 const AuthController = require('../controllers/authContoller');
 const path = require('path');
 
+//main
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'view', 'home.html'));
+});
+//logout
+router.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.log(err);
+            return res.redirect('/');
+        }
+        res.clearCookie('connect.sid');
+        res.redirect('/');
+    });
 });
 //login
 router.get('/login', (req, res) => { 
